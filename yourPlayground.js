5c01473ed003b22ac4596729ed8334626657c84b
sr.chandrashekar <chandru007.raju@gmail.com>
2022-12-17 23:48:02 +0530

    updated

--- yourPlayground.js
+++ yourPlayground.js
@@ -558,3 +558,17 @@ const fruits = ['apple','banana','orange','grape']
 
 // another program that i practised
 
+// const wordFrequency1 = (phrase) => {
+//     let frequency = {}
+//     words = phrase.split (' ')
+//     console.log(words)
+//    for (const word of words){
+//     if(word in frequency){
+//         frequency[word] = frequency[word] + 1
+//     }else{
+//         frequency[word] = 1
+//     }
+//    }
+//     return frequency
+// }
+// console.log(wordFrequency1('lol what lol'))
